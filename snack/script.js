54ba66e509be63ca27d415cd3328a6cfbb8f928b
Barbarubia <dnl.desimone@gmail.com>
2022-03-10 13:49:54 -0300

    creazione cartella snack con relativi files

---
+++ snack/script.js
@@ -0,0 +1,44 @@
+/*
+Snack 1:
+A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+Es:
+['pippo', 'PLUTO', 'Paperino'] => ['Pippo', 'Pluto', 'Paperino']
+*/
+console.log('----------');
+console.log('SNACK 1');
+console.log('----------');
+
+
+
+
+
+
+/*
+Snack 2:
+Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+Es:
+[
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+Crea un nuovo array con la lista dei mammiferi.
+*/
+console.log('----------');
+console.log('SNACK 2');
+console.log('----------');
+
+
+
+
+
+/*
+Snack 3:
+Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un’età.
+Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l’indicazione se può guidare, in base all’età.
+*/
+console.log('----------');
+console.log('SNACK 3');
+console.log('----------');
