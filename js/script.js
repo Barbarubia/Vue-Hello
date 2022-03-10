91089986327d2b5f90e8e35537603dc1406c984a
Barbarubia <dnl.desimone@gmail.com>
2022-03-10 11:35:42 -0300

    Vue Hello World h1+img

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const setting = {
+    el: '#app',
+    data: {
+        greeting: 'Hello',
+        name: 'World',
+        srcImg: 'img/world.png',
+        altImg: 'world',
+        classList: 'text-center'
+        }
+};
+const app = new Vue(setting);
